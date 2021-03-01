6fc46216771993ec7170ec108628923a0126b9a7
qdf666 <2430797451@qq.com>
2021-03-01 21:33:48 +0800

    分支初始化springboot项目

---
+++ peamall/src/main/java/com/qdf/peacemall/PeacemallApplication.java
@@ -0,0 +1,13 @@
+package com.qdf.peacemall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeacemallApplication {
+//chong
+    public static void main(String[] args) {
+        SpringApplication.run(PeacemallApplication.class, args);
+    }
+
+}
