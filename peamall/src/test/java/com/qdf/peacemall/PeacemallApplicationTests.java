3cc92370ef078ce9ac9292cf2c02c752201ab513
qdf666 <2430797451@qq.com>
2021-03-01 21:37:00 +0800

    分支初始化springboot项目

---
+++ peamall/src/test/java/com/qdf/peacemall/PeacemallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qdf.peacemall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeacemallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
